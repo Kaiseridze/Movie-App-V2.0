a75f287d96b548f7bdd275d9fc7dba9f1e3c3fb5
Kaiseridze <kaiseidze@gmail.com>
2022-04-13 23:55:34 +0300

    made 494 page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Favorites from './Pages/Favorites'
 import Home from './Pages/Home'
+import NotFound from './Pages/NotFound'
 
 function App() {
 	return (
 		<Routes>
+			<Route path='*' element={<NotFound />} />
 			<Route path='/' element={<Home />} />
 			<Route path='/favorites' element={<Favorites />} />
 		</Routes>
---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/Header"
+
+function NotFound() {
+	return (
+		<>
+			<Header />
+			<h1 className='error_text'>Увы, но страница не найдена, 404 :(((</h1>
+		</>
+	)
+}
+
+export default NotFound
